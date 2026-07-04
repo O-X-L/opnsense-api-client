e845ccb601d97e6293f3f9403b19788cd6d32cf7
Rath Pascal <rath@superstes.at>
2026-07-04 14:49:39 +0200

    test: update function-test script

--- scripts/functional_test.sh
+++ scripts/functional_test.sh
@@ -4,4 +4,10 @@ set -e
 
 cd "$(dirname "$0")/.."
 
+if [ -z "$TEST_FIREWALL" ] || [ -z "$TEST_API_CREDS" ]
+then
+  echo "ERROR: TEST_FIREWALL and TEST_API_CREDS have to be provided!"
+  exit 1
+fi
+
 python3 -m pytest -c pytest_functional.ini $@
